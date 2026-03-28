d1759b2eaa4719345950de725a666452d0e028b8
Nitya Narigiri <srinitya.narigiri03@gmail.com>
2026-03-28 15:40:36 +0530

    Winning logic

--- match_handler.go
+++ match_handler.go
@@ -10,6 +10,17 @@ import (
 
 const matchLabel = "tic-tac-toe"
 
+var winConditions = [8][3][2]int{
+	{{0, 0}, {0, 1}, {0, 2}}, // Row 1
+	{{1, 0}, {1, 1}, {1, 2}}, // Row 2
+	{{2, 0}, {2, 1}, {2, 2}}, // Row 3
+	{{0, 0}, {1, 0}, {2, 0}}, // Column 1
+	{{0, 1}, {1, 1}, {2, 1}}, // Column 2
+	{{0, 2}, {1, 2}, {2, 2}}, // Column 3
+	{{0, 0}, {1, 1}, {2, 2}}, // Diagonal \
+	{{0, 2}, {1, 1}, {2, 0}}, // Diagonal /
+}	
+
 type MatchHandler struct{}
 
 type MatchState struct {
@@ -129,6 +140,28 @@ func (m *MatchHandler) MatchLoop(ctx context.Context, logger runtime.Logger, db 
 				s.currentTurn = player
 				break
 			}
+		}
+
+		win := false
+		for _, condition := range winConditions {
+			if s.board[condition[0][0]][condition[0][1]] == mark &&
+			   s.board[condition[1][0]][condition[1][1]] == mark &&
+			   s.board[condition[2][0]][condition[2][1]] == mark {
+				win = true
+				logger.Info("Player %s wins!", s.players[userId].GetUsername())
+				break
+			}
+		}
+
+		if win {
+			result := map[string]string{
+				"winner": s.players[userId].GetUsername()
+			}
+			resultBytes, _ := json.Marshal(result)
+			dispatcher.BroadcastMessage(2, resultBytes, nil, nil, true)
+			// logger.Info("Match ended - winner declared")
+
+			return nil
 		}
 
 		boardState, _ := json.Marshal(s.board)
