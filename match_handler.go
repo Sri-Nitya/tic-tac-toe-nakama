8f6285aca250ae5169fa53b5f375247b9e731e58
Nitya Narigiri <srinitya.narigiri03@gmail.com>
2026-03-29 16:15:58 +0530

    improve server-authoritative match handling and game result broadcasts

--- match_handler.go
+++ match_handler.go
@@ -28,11 +28,31 @@ type MatchState struct {
 	marks  map[string]string
 	board  [3][3]string
 	currentTurn string
+	gameOver    bool
+	winner      string
 }
 
 type Move struct {
 	Row int `json:"row"`
 	Col int `json:"col"`	
+}
+
+func broadcastState(dispatcher runtime.MatchDispatcher, s *MatchState) {
+	response := map[string]interface{}{
+		"board":    s.board,
+		"turn":     s.marks[s.currentTurn],
+		"players":  s.marks,
+		"gameOver": s.gameOver,
+		"winner":   s.winner,
+	}
+
+	responseBytes, _ := json.Marshal(response)
+	_ = dispatcher.BroadcastMessage(1, responseBytes, nil, nil, true)
+}
+
+func broadcastResult(dispatcher runtime.MatchDispatcher, payload map[string]interface{}) {
+	resultBytes, _ := json.Marshal(payload)
+	_ = dispatcher.BroadcastMessage(2, resultBytes, nil, nil, true)
 }
 
 func (m *MatchHandler) MatchInit(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, params map[string]interface{}) (interface{}, int, string) {
@@ -48,8 +68,6 @@ func (m *MatchHandler) MatchInit(ctx context.Context, logger runtime.Logger, db 
 }
 
 func (m *MatchHandler) MatchJoinAttempt(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, dispatcher runtime.MatchDispatcher, tick int64, state interface{}, presence runtime.Presence, metadata map[string]string) (interface{}, bool, string) {
-
-	// logger.Info("JOIN ATTEMPT: %s", presence.GetUsername())
 
 	if len(state.(*MatchState).players) >= 2 {
 		logger.Info("JOIN ATTEMPT REJECTED: MATCH FULL")
@@ -69,15 +87,14 @@ func (m *MatchHandler) MatchJoin(ctx context.Context, logger runtime.Logger, db 
 		if len(s.players) == 1 {
 			s.marks[p.GetUserId()] = "X"
 			s.currentTurn = p.GetUserId()
-			logger.Info("First turn assigned to player: %s", s.players[p.GetUserId()].GetUsername())
+			logger.Info("First player joined: %s as X", p.GetUsername())
 		} else if len(s.players) == 2 {
 			s.marks[p.GetUserId()] = "O"
+			logger.Info("Second player joined: %s as O", p.GetUsername())
 		}
-
-		logger.Info("Player joined: %s, assigned mark: %s", p.GetUsername(), s.marks[p.GetUserId()])
-		// logger.Info("Number of players in match: %d", len(s.players))
 	}
 
+	broadcastState(dispatcher, s)
 	return s
 }
 
@@ -85,9 +102,36 @@ func (m *MatchHandler) MatchLeave(ctx context.Context, logger runtime.Logger, db
 
 	s := state.(*MatchState)
 
+	if s.gameOver {
+		for _, p := range presences {
+			delete(s.players, p.GetUserId())
+		}
+		return s
+	}
+
 	for _, p := range presences {
-		delete(s.players, p.GetUserId())
-		logger.Info("Player left: %s", p.GetUsername())
+		leftUserID := p.GetUserId()
+		leftUsername := p.GetUsername()
+
+		delete(s.players, leftUserID)
+
+		logger.Info("Player left: %s", leftUsername)
+
+		if len(s.players) == 1 {
+			for remainingUserID, remainingPresence := range s.players {
+				s.gameOver = true
+				s.winner = remainingPresence.GetUsername()
+
+				broadcastResult(dispatcher, map[string]interface{}{
+					"type":   "disconnect",
+					"winner": s.winner,
+					"message": "Opponent disconnected",
+				})
+
+				logger.Info("Opponent disconnected. Winner: %s (%s)", s.winner, remainingUserID)
+				break
+			}
+		}
 	}
 
 	return s
@@ -98,6 +142,11 @@ func (m *MatchHandler) MatchLoop(ctx context.Context, logger runtime.Logger, db 
 	s := state.(*MatchState)
 
 	for _, msg := range messages {
+		if len(s.players) < 2 || s.gameOver {
+			logger.Info("Ignoring move")
+			continue
+		}
+
 		var move Move
 		err := json.Unmarshal(msg.GetData(), &move)
 		if err != nil {
@@ -126,20 +175,38 @@ func (m *MatchHandler) MatchLoop(ctx context.Context, logger runtime.Logger, db 
 		}
 
 		mark := s.marks[userId]
+		if mark == "" {
+			logger.Info("Player %s has no assigned mark", userId)
+			continue
+		}
 
-		// Update board state
 		s.board[move.Row][move.Col] = mark
 		logger.Info("Player %s placed %s at (%d, %d)",userId, mark, move.Row, move.Col)
 
-		// Switch turn
-		for player := range s.players {
-			if player != userId {
-				s.currentTurn = player
+		win := false
+		for _, condition := range winConditions {
+			if s.board[condition[0][0]][condition[0][1]] == mark &&
+				s.board[condition[1][0]][condition[1][1]] == mark &&
+				s.board[condition[2][0]][condition[2][1]] == mark {
+				win = true
 				break
 			}
 		}
 
-		// No moves left, it's a draw
+		if win {
+			s.gameOver = true
+			s.winner = s.players[userId].GetUsername()
+
+			broadcastState(dispatcher, s)
+			broadcastResult(dispatcher, map[string]interface{}{
+				"type":   "win",
+				"winner": s.winner,
+			})
+
+			logger.Info("Player %s wins!", s.winner)
+			return s
+		}
+
 		isBoardFull := true
 		for i := 0; i < 3; i++ {
 			for j := 0; j < 3; j++ {
@@ -151,42 +218,29 @@ func (m *MatchHandler) MatchLoop(ctx context.Context, logger runtime.Logger, db 
 		}
 
 		if isBoardFull {
-			logger.Info("Board is full, it's a draw!")
+			s.gameOver = true
+
+			broadcastState(dispatcher, s)
+			broadcastResult(dispatcher, map[string]interface{}{
+				"type":    "draw",
+				"message": "It's a draw",
+			})
+
+			logger.Info("Board is full, it's a draw")
+			return s
 		}
 
-
-		win := false
-		for _, condition := range winConditions {
-			if s.board[condition[0][0]][condition[0][1]] == mark &&
-			   s.board[condition[1][0]][condition[1][1]] == mark &&
-			   s.board[condition[2][0]][condition[2][1]] == mark {
-				win = true
-				logger.Info("Player %s wins!", s.players[userId].GetUsername())
+		for player := range s.players {
+			if player != userId {
+				s.currentTurn = player
 				break
 			}
 		}
 
-		if win {
-			result := map[string]string{
-				"winner": s.players[userId].GetUsername(),
-			}
-			resultBytes, _ := json.Marshal(result)
-			dispatcher.BroadcastMessage(2, resultBytes, nil, nil, true)
-			// logger.Info("Match ended - winner declared")
-
-			return nil
-		}
-
-		response := map[string]interface{}{
-			"board": s.board,
-			"turn": s.marks[s.currentTurn], // send X or O instead of userId
-			"players": s.marks,
-		}
-
-		responseBytes, _ := json.Marshal(response)
-		dispatcher.BroadcastMessage(1, responseBytes, nil, nil, true)
+		broadcastState(dispatcher, s)
 	}
-	return state
+
+	return s
 }
 
 func (m *MatchHandler) MatchTerminate(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, dispatcher runtime.MatchDispatcher, tick int64, state interface{}, graceSeconds int) interface{} {
