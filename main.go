4790908d9a4a1edb8c5fef6cd3e6ad33bd51f7a8
Nitya Narigiri <srinitya.narigiri03@gmail.com>
2026-03-30 10:59:20 +0530

    Automatic matchmaking

--- main.go
+++ main.go
@@ -38,5 +38,31 @@ func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runti
 		logger.Error("NITYA TEST: ERROR REGISTERING RPC: %v", err)
 		return err
 	}
+
+	err = initializer.RegisterRpc("quick_match", func(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+		minSize := 0
+		maxSize := 2
+		matches, err := nk.MatchList(ctx, 10, true, matchLabel, &minSize, &maxSize, "")
+		if err != nil {
+			logger.Error("ERROR LISTING MATCHES: %v", err)
+			return "", err
+		}
+
+		for _, match := range matches {
+			if match.Size < 2 {
+				response := map[string]string{"matchId": match.MatchId}
+				responseBytes, _ := json.Marshal(response)
+				return string(responseBytes), nil
+			}
+		}
+		matchId, err := nk.MatchCreate(ctx, matchLabel, nil)
+		if err != nil {
+			logger.Error("ERROR CREATING MATCH: %v", err)
+			return "", err
+		}
+		response := map[string]string{"matchId": matchId}
+		responseBytes, _ := json.Marshal(response)
+		return string(responseBytes), nil
+	})
 	return nil
 }
